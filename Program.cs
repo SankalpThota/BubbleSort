76729ba48cfaf65f2add1daf05421ee2de22afa3
Sai Sankalp Thota <saisankalp@usf.edu>
2024-02-28 10:57:30 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        int[] bubble_sort = { 5, 72, 3, 14, 7, 1 };
+        Console.WriteLine("Original Array For Bubble Sort ");
+        for (int i = 0; i < bubble_sort.Length; i++)
+        {
+            Console.Write("  " + bubble_sort[i]);
+        }
+
+        Console.WriteLine("");
+        Console.WriteLine("Array after Execution ");
+        Bubble_Sort(bubble_sort);
+        for (int i = 0; i < bubble_sort.Length; i++)
+        {
+            Console.Write("  " + bubble_sort[i]);
+        }
+        Console.WriteLine("");
+        Console.WriteLine("Press Anything to exit");
+        Console.ReadKey(true);
+
+        static void Bubble_Sort(int[] bs_arr)
+        {
+
+            for (int i = 0; i < bs_arr.Length - 1; i++)
+            {
+                for (int x = 0; x < bs_arr.Length - i - 1; x++)
+                {
+
+                    if (bs_arr[x] > bs_arr[x + 1])
+                    {
+                        int temp = bs_arr[x];
+                        bs_arr[x] = bs_arr[x + 1];
+                        bs_arr[x + 1] = temp;
+                    }
+                }
+            }
+        }
+
+    }
+
+}
+
+
